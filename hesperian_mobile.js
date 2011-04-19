daccd36e52e2e436a8af0683aefb6ede881937ec
Saq Imtiaz <saq.imtiaz@gmail.com>
2011-04-19 13:04:19 +0200

    seeded with Matthew Litwin's sample code

---
+++ hesperian_mobile.js
@@ -0,0 +1,9 @@
+// hesperian_mobile.js
+
+// iPhone / Mobile Safari workarounds
+function isiPhone(){
+    return ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)));
+}
+
+
+
