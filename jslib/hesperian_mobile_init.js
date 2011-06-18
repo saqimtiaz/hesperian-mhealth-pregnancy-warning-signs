a9ebea78573a8a1ea088e12909e7bcdd7dc6dc62
saqimtiaz <saq.imtiaz@gmail.com>
2011-06-18 21:16:31 +0200

    Refactored binding of swipe events to minimize DOM traversal

--- jslib/hesperian_mobile_init.js
+++ jslib/hesperian_mobile_init.js
@@ -8,7 +8,8 @@ function isiPhone(){
 };
 
 $(document).bind("mobileinit", function(){
-  $.mobile.defaultTransition = "none";
+	$.mobile.defaultTransition = "none";
+	//$.mobile.hashListeningEnabled = false;
 });
 
 $("div:jqmData(role='page')").live('pagebeforecreate',function(event){
@@ -38,18 +39,19 @@ $("div:jqmData(role='page')").live('pagebeforecreate',function(event){
 
 //binds swipe events to the specified elements and maps them to clicks on the previous and next links based on them having the appropriate class
 function swipeToClick(el) {
+	//console.log("swiper");
 	$(el).bind("swiperight swipeleft", function(event) {
 		//console.log($("a.seq-nav-button-left:first",this));
 		event.preventDefault();
 		if (event.type == "swipeleft") {
 			//$("a.seq-nav-button-right:first:not(:disabled)",this).click();
-			var href = $("a.seq-nav-button-right:first:not(:disabled)",this).attr("href");
+			var href = $("a.seq-nav-button-right:first",this).attr("href");
 			if (href != "javascript:;")
 				$.mobile.changePage(href,"none");
 		}
 		else if (event.type == "swiperight") {
 			//$("a.seq-nav-button-left:first:not(:disabled)",this).click();
-			var href = $("a.seq-nav-button-left:first:not(:disabled)",this).attr("href");
+			var href = $("a.seq-nav-button-left:first",this).attr("href");
 			//console.log(href);
 			if (href != "javascript:;")
 				$.mobile.changePage(href,"none");
@@ -60,9 +62,12 @@ function swipeToClick(el) {
 
 //swiping would need to be selectively added to pages where we wanted it
 $("div:jqmData(role='page')").live("pagecreate",function(event) {
+	//console.log(this);
 	var page = $(this);
 	//pass page to bind swipe after filtering it for pages containing a div with class sequence bar, this identifies that swiping is to be enabled.
-	swipeToClick(page.has("div.sequence-bar"));
+	//console.log(page.attr("swipe"));
+	if (page.attr("swipe") == "true")
+		swipeToClick(page);
 });
 
 // jquery mobile configuration
