2fc51f7ededad8633c9495ab44ea8af55be45da0
saqimtiaz <saq.imtiaz@gmail.com>
2011-07-24 15:20:11 +0200

    Tweaked swipe event parameters to reduce incidence of accidental swiping when scrolling

--- jslib/hesperian_mobile.js
+++ jslib/hesperian_mobile.js
@@ -3,4 +3,8 @@
 // Override swipe verticalDistanceThreshold (75) to be less sensitive.
 // Note that this is a hesperian mobile specific patch to JQM - but
 // should be harmless for a non-patched version.
-$.event.special.swipe.verticalDistanceThreshold = 125;
+$.event.special.swipe.verticalDistanceThreshold = 125; // Swipe vertical displacement must be less than this.
+$.event.special.swipe.scrollSupressionThreshold= 20; // More than this horizontal displacement, and we will suppress scrolling.
+$.event.special.swipe.durationThreshold= 1000; // More time than this, and it isn't a swipe.
+$.event.special.swipe.horizontalDistanceThreshold= 60;  // Swipe horizontal displacement must be more than this.
+
