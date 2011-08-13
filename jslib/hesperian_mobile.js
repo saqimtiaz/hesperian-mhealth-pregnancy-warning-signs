323f6ab0df3fc4bfa1f81a3373e62d893dc11f5d
mlitwin <mlitwin@kcptech.com>
2011-08-13 15:13:30 -0700

    Move to JQM 1.0b2 / jquery-1.6.2

--- jslib/hesperian_mobile.js
+++ jslib/hesperian_mobile.js
@@ -1,8 +1,6 @@
 // hesperian_mobile.js
 
-// Override swipe verticalDistanceThreshold (75) to be less sensitive.
-// Note that this is a hesperian mobile specific patch to JQM - but
-// should be harmless for a non-patched version.
+// Override swipe verticalDistanceThreshold (75) and friends to be less sensitive.
 $.event.special.swipe.verticalDistanceThreshold = 125; // Swipe vertical displacement must be less than this.
 $.event.special.swipe.scrollSupressionThreshold= 20; // More than this horizontal displacement, and we will suppress scrolling.
 $.event.special.swipe.durationThreshold= 1000; // More time than this, and it isn't a swipe.
