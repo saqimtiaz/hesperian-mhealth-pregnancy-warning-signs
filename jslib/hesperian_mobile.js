3199319e5a3b10ab680e373f3244bc96d5dfac29
saqimtiaz <saq.imtiaz@gmail.com>
2011-08-14 12:34:54 +0200

    Disabled floating 'more' text on menu screens except How Tos

--- jslib/hesperian_mobile.js
+++ jslib/hesperian_mobile.js
@@ -6,20 +6,24 @@ $.event.special.swipe.scrollSupressionThreshold= 20; // More than this horizonta
 $.event.special.swipe.durationThreshold= 1000; // More time than this, and it isn't a swipe.
 $.event.special.swipe.horizontalDistanceThreshold= 60;  // Swipe horizontal displacement must be more than this.
 
+
+/* start of code for displaying an indicator that you can scroll to see more content */
 function getHiddenHeight() {
 		return $(document).height() - $(document).scrollTop() - $(window).height();
 }
 
-/* start of code for displaying an indicator that you can scroll to see more content */
-
-//check jqm fixed header code for a better way of doing this
+// TODO: check jqm fixed header code for a better cross platform way of doing this
 function setBottomPosition (el) {
 	$(el).css("top",window.innerHeight + window.scrollY - 44 + "px");
 }
 
 function showKeepReadingText(id) {
+	// TODO: should really use an attribute on the page container div instead, ala jqm
+	var menus = ["home","Staying_healthy_during_pregnancy", "Danger_Signs_During_Pregnancy", "Danger_Signs_During_Birth", "Danger_Signs_After_Birth"];
+	if ($.inArray($.mobile.activePage[0].id,menus) != -1) {
+		return false;
+	}
 	var hidden = getHiddenHeight();
-	//alert(hidden);
 	if (hidden > 20) {
 		setBottomPosition(id);
 		$(id).show();
