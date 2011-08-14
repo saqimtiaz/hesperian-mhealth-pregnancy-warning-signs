2acfcbc272a3b63b0c076d67dfcde24ab668e6be
saqimtiaz <saq.imtiaz@gmail.com>
2011-08-14 11:18:17 +0200

    Committed initial changes based on alpha test feedback. All of these changes are still raw: Added code to display a more text when there is content below the fold that can be seen by scrolling. Still needs to be tweaked to disallow display on menu pages. Started working on removing the header title being a home button and added a dedicated home button with a house symbol.

--- jslib/hesperian_mobile.js
+++ jslib/hesperian_mobile.js
@@ -8,3 +8,41 @@ $.event.special.swipe.scrollSupressionThreshold= 20; // More than this horizonta
 $.event.special.swipe.durationThreshold= 1000; // More time than this, and it isn't a swipe.
 $.event.special.swipe.horizontalDistanceThreshold= 60;  // Swipe horizontal displacement must be more than this.
 
+function getHiddenHeight() {
+		return $(document).height() - $(document).scrollTop() - $(window).height();
+}
+
+/* start of code for displaying an indicator that you can scroll to see more content */
+
+//check jqm fixed header code for a better way of doing this
+function setBottomPosition (el) {
+	$(el).css("top",window.innerHeight + window.scrollY - 44 + "px");
+}
+
+function showKeepReadingText(id) {
+	var hidden = getHiddenHeight();
+	//alert(hidden);
+	if (hidden > 20) {
+		setBottomPosition(id);
+		$(id).show();
+		$(id).fadeIn("slow");
+	} else {
+		$(id).hide();
+	}
+}
+
+$("div:jqmData(role='page')").live("pageshow",function(event) {
+	showKeepReadingText("#hm-keepreading");
+});
+
+$("div:jqmData(role='page')").live("pagehide",function(event) {
+	$("#hm-keepreading").hide();
+});
+
+$(window).bind("scrollstart",function(event){
+	$("#hm-keepreading").hide();
+});
+
+$(window).bind("scrollstop",function(event){
+	showKeepReadingText("#hm-keepreading");
+});
